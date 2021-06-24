eba90b2a9c6d53775454c16b06fd97ec8e00110c
Le <nghi.le@bigdutchman.com>
2021-06-24 11:51:23 +0700

    push

---
+++ datasource/simple-datasource.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+function path(file) {
+  return "data/" + file;
+}
+
+exports.loadAccount = function (callback) {
+  fs.readFile(path("account.json"), function (err, data) {
+    var account = JSON.parse(data);
+    callback(account);
+  });
+};
--- main.js
+++ main.js
@@ -17,6 +17,34 @@ router.get("/", function (request, response) {
   controller("home").get(request, response);
 });
 
+var datasource = require("./datasource/simple-datasource");
+var model = require("./model/model").create(datasource);
+
+// Setup web app
+
+app.use(require("cookie-parser")());
+
+
+var urlencodeParser = require("body-parser").urlencoded({ extended: false });
+
+router.get("/", function (request, response) {
+  controller("home").get(request, response, webconfig, model);
+});
+
+router.get("/login", function (request, response) {
+  controller("login").get(request, response, webconfig, model);
+});
+
+router.post("/login", urlencodeParser, function (request, response) {
+  console.log('success')
+  controller("login").post(request, response, webconfig, model);
+});
+
+router.get("/logout", function (request, response) {
+  console.log('success')
+  controller("logout").get(request, response, webconfig);
+});
+
 app.use(webconfig.root, router);
 
 app.listen(8080, function () {
